a0522addbed5ae5aedd1a3b4d8f533dfd287c807
soila.torome <soila.torome@student.moringaschool.com>
2022-09-01 16:10:52 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,15 @@
 const init = () => {
-  
-}
+    let getForm = document.querySelector("form");
+	getForm.addEventListener("submit", (e) => {
+		e.preventDefault();
+		let getSearch = document.querySelector("#searchByID").value;
+		fetch("http://localhost:3000/movies/" + getSearch)
+			.then((res) => res.json())
+			.then((data) => {
+				let getMovie = document.querySelector("#movieDetails");
+				getMovie.innerHTML = data.title;
+			});
+	});
+};
 
 document.addEventListener('DOMContentLoaded', init);
